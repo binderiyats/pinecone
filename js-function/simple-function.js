98ea69a71da241315ac0bffd64bf1e02e662632e
Binderiya <binderiyatsogthers@gmail.com>
2022-12-06 16:08:57 +0800

    added

---
+++ js-function/simple-function.js
@@ -0,0 +1,15 @@
+function fullName(firstName, lastName) {
+        return firstName + lastName;
+      }
+      console.log(fullName("Binderiya ", "Tsogt "));
+
+      //Area of a rectangle
+function area(a, b) {
+        return a * b;
+      }
+      console.log(area(12, 2));
+      //Perimeter of a rectangle
+      function perimeter(a, b) {
+        return (a + b) * 2;
+      }
+      console.log(perimeter(2, 2));

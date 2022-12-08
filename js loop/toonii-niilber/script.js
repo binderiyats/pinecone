91fc44a550f02b3ccbc46a98aa9d010721987650
binderiyats <binderiyatsogthers@gmail.com>
2022-12-08 22:04:23 +0800

    updated

--- js loop/toonii-niilber/script.js
+++ js loop/toonii-niilber/script.js
@@ -1,4 +1,5 @@
-let n = Number(window.prompt("too bichne uu"));
+// let n = Number(window.prompt("too bichne uu"));
+let n = 12;
       let niilber = 0;
       for (let i = 1; i <= n; i++) {
         niilber = niilber + i;

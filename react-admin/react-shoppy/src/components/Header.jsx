b9812f6aff5ebd384e81ca8c56b4efe6a269e422
binderiyats <binderiyatsogthers@gmail.com>
2023-01-05 13:28:15 +0800

    new project

---
+++ react-admin/react-shoppy/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import HeaderContact from "./Header/HeaderContact";
+import HeaderSearch from "./Header/HeaderSearch";
+import Navbar from "./Header/Navbar";
+
+export default function Header(){
+    return(
+        <>
+        </>
+    )
+}

0506a71c1c37fab04817d98e375d01d51cd152c0
Binderiya <binderiyatsogthers@gmail.com>
2023-01-26 17:52:55 +0800

    updated todo state

--- react-admin/admin/src/State.jsx
+++ react-admin/admin/src/State.jsx
@@ -1,60 +1,91 @@
-// import { useState } from "react";
-
-// export const Todo = () => {
-//   const [count, setCount] = useState(0);
-
-//   const counter = () => {
-//     setCount(count + 1);
-//   };
-
-//   return (
-//     <>
-//       <button onClick={counter}>Count</button>
-//       <p>{count}</p>
-//     </>
-//   );
-// };
 import { useState } from "react";
 
-const styles = {
-  wrapper: {
-    minHeight: "100vh",
-    maxWidth: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-  },
-};
 export default function State() {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState("");
-
-  const addTodo = () => {
-    const newList = [...todos, input];
-    setTodos(newList);
-    setInput("");
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const styles = {
+    wrapper: {
+      minHeight: "100vh",
+      maxWidth: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+    },
   };
 
-  const removeByIndex = (id) => {
-    let filteredTodo = todos.filter((todo, index) => {
-      return id !== index;
-    });
-    setTodos(filteredTodo);
+  const handlechange = (event) => {
+    setNewTask(event.target.value);
   };
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+  };
+
+  const deleteTask = () => {};
 
   return (
-    <div style={styles.wrapper}>
-      <header>todos</header>
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={addTodo}>Add</button>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index} onClick={() => removeByIndex(index)}>
-            {todo}
-          </li>
-        ))}
-      </ul>
+    <div className="app" style={styles.wrapper}>
+      <div className="addTask">
+        <input onChange={handlechange} />
+        <button onClick={addTask}>Add task</button>
+      </div>
+      <div className="list">
+        <div>
+          <ul>
+            {todoList.map((task) => {
+              return <li>{task}</li>;
+            })}
+          </ul>
+          <button onClick={deleteTask}> x </button>
+        </div>
+      </div>
     </div>
   );
 }
+
+// import { useState } from "react";
+
+// const styles = {
+//   wrapper: {
+//     minHeight: "100vh",
+//     maxWidth: "100%",
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     flexDirection: "column",
+//   },
+// };
+// export default function State() {
+//   const [todos, setTodos] = useState([]);
+//   const [input, setInput] = useState("");
+
+//   const addTodo = () => {
+//     const newList = [...todos, input];
+//     setTodos(newList);
+//     setInput("");
+//   };
+
+//   const removeByIndex = (id) => {
+//     let filteredTodo = todos.filter((todo, index) => {
+//       return id !== index;
+//     });
+//     setTodos(filteredTodo);
+//   };
+
+//   return (
+//     <div style={styles.wrapper}>
+//       <header>todos</header>
+//       <input value={input} onChange={(e) => setInput(e.target.value)} />
+//       <button onClick={addTodo}>Add</button>
+//       <ul>
+//         {todos.map((todo, index) => (
+//           <li key={index} onClick={() => removeByIndex(index)}>
+//             {todo}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }

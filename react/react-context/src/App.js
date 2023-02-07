35c9c65b7119dc65c499d4039f78ce91e56ccbd4
Binderiya <binderiyatsogthers@gmail.com>
2023-02-07 17:28:22 +0800

    useContext-switch

---
+++ react/react-context/src/App.js
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import Switch from "./utils/Switch.jsx";
+
+export const ThemeContext = createContext("");
+
+function App() {
+  const [theme, setTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div className={"wrapper " + theme}>
+        <button
+          className="dark-mode-toggle toggler"
+          aria-hidden="true"
+          onClick={() => {
+            setTheme(theme === "light" ? "dark" : "light");
+          }}
+        >
+          <Switch />
+        </button>
+
+        <h1>My blog</h1>
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
